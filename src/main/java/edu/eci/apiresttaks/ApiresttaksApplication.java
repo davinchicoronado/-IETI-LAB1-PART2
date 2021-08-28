2eac76eddfc9c2d3b696aec6e8a707948e805b4d
David Coronado <david.coronado@escuelaing.edu.co>
2021-08-27 19:24:37 -0500

    create project with spring initializr

---
+++ src/main/java/edu/eci/apiresttaks/ApiresttaksApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.apiresttaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiresttaksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiresttaksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/apiresttaks/ApiresttaksApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.apiresttaks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiresttaksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
